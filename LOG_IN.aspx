59d8526c94ba6e252d99f974018132a98fa35a47
Mickey7 <mohammedsayedomar@yahoo.com>
2016-04-28 17:34:04 +0200

    Added files via upload
    Log in completed with all validation .. just add the chief page when its done

---
+++ LOG_IN.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LOG_IN.aspx.cs" Inherits="WebApplication1.LOG_IN" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lbel_username" runat="server" Text="Username:"></asp:Label>
+        <asp:TextBox ID="textBox_username" runat="server"></asp:TextBox>
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" ControlToValidate="textBox_username" runat="server" ErrorMessage="Only Numbers allowed" ValidationExpression="\d+"></asp:RegularExpressionValidator><b />
+
+
+        <asp:Label ID="label_pass" runat="server" Text="Password:"></asp:Label>
+        <asp:TextBox ID="textBox_pass" runat="server" TextMode="Password"></asp:TextBox>
+        
+         <b />
+        <asp:Button ID="button_login" runat="server" Text="LOG IN" OnClick="button_login_Click" /> 
+        <asp:Literal ID="feedback" runat="server"></asp:Literal>
+    </div>
+    </form>
+</body>
+</html>
---
+++ LOG_IN.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class LOG_IN : System.Web.UI.Page
+    {
+        Controller controller;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button_login_Click(object sender, EventArgs e)
+        {
+            if (textBox_username.Text == "" && textBox_pass.Text == "")
+            {
+                //  MessageBox.Show("Please enter username & password");
+                feedback.Text = "Please enter username & password";
+                return;
+            }
+            else if (textBox_username.Text == "")
+            {
+
+                feedback.Text = "Please enter username ";
+                return;
+            }
+            else if (textBox_pass.Text == "")
+            {
+
+                feedback.Text = "Please enter password ";
+                return;
+            }
+            
+            controller = new Controller();
+            DataTable usertable = controller.CheckUserPass(Convert.ToInt32(textBox_username.Text), textBox_pass.Text);
+            string userType;
+            if (usertable == null)
+            {
+                feedback.Text = "Password or Username isn't correct";
+                return;
+            }
+            else
+            {
+                userType = (usertable.Rows[0][0]).ToString();
+            }
+
+            if (userType == "Admin")//Admin
+            {
+                Server.Transfer("Manager.aspx", true);
+            }
+            else if (userType == "Customer")
+            {
+                Server.Transfer("Customer.aspx", true);
+            }
+            else//chef 7aqer
+            {
+                //Server.Transfer("Manager.aspx", true);
+            }
+        }
+    }
+}
