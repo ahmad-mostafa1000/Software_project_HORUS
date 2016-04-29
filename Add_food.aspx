336025a058760ee2045304d3f8b71d93b209d1eb
savehillkill <eng_faresomar@yahoo.com>
2016-04-29 13:58:24 +0200

    Added files via upload

---
+++ Add_food.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Add_food.aspx.cs" Inherits="WebApplication1.Add_food" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+      <asp:Label ID="Label1_food" runat="server" Text="Food"></asp:Label>
+        <asp:TextBox ID="TextBox1_food" runat="server"></asp:TextBox>
+          <br />
+        <asp:Label ID="Label2_price" runat="server" Text="Price"></asp:Label>
+        <asp:TextBox ID="TextBox2price" runat="server"></asp:TextBox>
+          <br />
+        <asp:Label ID="Label3_estimatedtime" runat="server" Text="Estimated Time"></asp:Label>
+        <asp:TextBox ID="TextBox3time" runat="server"></asp:TextBox>
+       <br />
+       <br />
+        <asp:Button ID="Button_addmeal" runat="server" Text="Add Meal" onclick="Button_addmeal_Click"/>
+
+        <asp:Literal ID="feedback" runat="server"></asp:Literal>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Add_food.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+
+    public partial class Add_food : System.Web.UI.Page
+    {
+        Controller controller;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_addmeal_Click(object sender, EventArgs e)
+        {
+            controller = new Controller();
+
+            if (TextBox1_food.Text == "" || TextBox2price.Text == "" || TextBox3time.Text == "")
+            {
+                feedback.Text = "PLEASE, MAKE SURE YOU ENTERED ALL VALUES RIGHT";
+            }
+            else
+            {
+                feedback.Text = "";
+
+                int r = controller.add_meal(TextBox1_food.Text, Convert.ToSingle(TextBox2price.Text), Convert.ToSingle(TextBox3time.Text));//????????????
+
+                if (r != 0)
+                {
+                    feedback.Text = "DONE";
+                }
+                else
+                {
+                    feedback.Text = "Contact Your DBMS Manager";
+                }
+            }
+        }
+    }
+}
